b5aaf2eb4d475f59c514eec007fbacac1adf19dd
ewilbe5944 <32745881+ewilbe5944@users.noreply.github.com>
2018-03-30 13:32:10 -0400

    Add files via upload

---
+++ waves animation.py
@@ -0,0 +1,72 @@
+# Imports
+import pygame
+import random
+
+# Initialize game engine
+pygame.init()
+
+# Window
+SIZE = (800, 600)
+TITLE = "Picture"
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption(TITLE)
+
+# Timer
+clock = pygame.time.Clock()
+refresh_rate = 30
+
+# Colors
+GREEN = (0, 225, 0)
+WHITE = (255, 255, 255)
+DBLUE = (0, 0, 255)
+YELLOW = (255, 255, 175)
+LBLUE = (0, 225, 204)
+GRAY = (166, 166, 166)
+DGREEN = (0, 51, 0)
+BROWN = (102, 51, 0)
+
+
+# Make trees
+num_trees = 3
+trees = []
+for i in range(num_trees):
+    x = random.randrange(0, 800)
+    y = 300
+    loc = [x, y]
+    trees.append(loc)
+
+def draw_tree(loc):
+    x = loc[0]
+    y = loc[1]
+
+    pygame.draw.rect(screen, BROWN, [x + 20, y + 70, 20, 40], 0)
+    pygame.draw.ellipse(screen, DGREEN, [x, y, 60, 80], 0)
+   
+
+#game loop
+done = False
+
+while not done:
+    # Event processing
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    #sky
+    screen.fill(LBLUE)
+    #ground
+    pygame.draw.rect(screen, GREEN, [0, 400, 800, 200], 0)
+    #trees
+    for t in trees:
+        draw_tree(t)
+
+
+
+
+
+                        
+    # Update screen
+    pygame.display.flip()
+    clock.tick(refresh_rate)
+
+# Close window on quit
+pygame.quit()
